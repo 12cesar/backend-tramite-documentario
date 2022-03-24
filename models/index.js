efba6454485e516729bdf130892d7e53dad5b611
julio cesar calderon galindo <julcescalgalty@gmail.com>
2022-03-24 14:00:57 -0500

    creando el index de models

--- models/index.js
+++ models/index.js
@@ -1,9 +1,56 @@
-
 const Server = require("./server");
 const Usuario = require("./user");
 const Cargo = require("./cargo");
+const Codigodocumento = require("./codigodocumento");
+const Derivacionexterno = require("./derivacionexterno");
+const Derivacioninterno = require("./derivacioninterno");
+const Direccion = require("./direccion");
+const Documentoadjuntarexterno = require("./documentoadjuntar-externo");
+const Documentoadjuntarinterno = require("./documentoadjuntar-interno");
+const Documentoexterno = require("./documentoexterno");
+const Documentofirmaexterno = require("./documentofirma-externo");
+const Documentofirmainterno = require("./documentofirma-interno");
+const Documentointerno = require("./documentointerno");
+const Estadodocumento = require("./estadodocumento");
+const Estadotramite = require("./estadotramite");
+const Estructura = require("./estructura");
+const Personajuridica = require("./personajuridica");
+const Personanatural = require("./personanatural");
+const Recepcionexterno = require("./recepcionexterno");
+const Recepcioninterno = require("./recepcioninterno");
+const Remitente = require("./remitente");
+const Tipodocumento = require("./tipodocumento");
+const Tipopersona = require("./tipopersona");
+const Tramiteexterno = require("./tramite-externo");
+const Tramiteinterno = require("./tramite-interno");
+const Tramiteadjuntarexterno = require("./tramiteadjuntar-externo");
+const Userarea = require("./userarea");
 module.exports = {
   Server,
   Usuario,
   Cargo,
+  Codigodocumento,
+  Derivacionexterno,
+  Derivacioninterno,
+  Direccion,
+  Documentoadjuntarexterno,
+  Documentoadjuntarinterno,
+  Documentoexterno,
+  Documentofirmaexterno,
+  Documentofirmainterno,
+  Documentointerno,
+  Estadodocumento,
+  Estadotramite,
+  Estructura,
+  Personajuridica,
+  Personanatural,
+  Recepcionexterno,
+  Recepcioninterno,
+  Remitente,
+  Tipodocumento,
+  Tipopersona,
+  Tramiteexterno,
+  Tramiteinterno,
+  Tramiteadjuntarexterno,
+  Userarea
 };
