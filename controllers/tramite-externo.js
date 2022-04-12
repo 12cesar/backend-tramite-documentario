ccd223c0e35b3376aa04ee8a34d8dad45fc0e950
julio cesar calderon galindo <julcescalgalty@gmail.com>
2022-04-12 14:10:36 -0500

    creando crud tramite externo

--- controllers/tramite-externo.js
+++ controllers/tramite-externo.js
@@ -1,38 +1,51 @@
-const { response,request } = require("express");
+const { response, request } = require("express");
+const { Tramiteexterno } = require("../models");
 
-
-const getTramiteExternos =async(req=request,res=response)=>{
+const getTramiteExternos = async (req = request, res = response) => {
+  res.json({
+    ok: true,
+  });
+};
+const getTramiteExterno = async (req = request, res = response) => {
+  res.json({
+    ok: true,
+  });
+};
+const postTramiteExterno = async (req = request, res = response) => {
+  try {
+    const data = req.body;
+    const countExter = await Tramiteexterno.count();
+    data.codigo = `${countExter+1}`;
+    data.estadoTramite = 1;
+    const tramiteexter = new Tramiteexterno(data);
     res.json({
-        ok:true
-    })
-}
-const getTramiteExterno =async(req=request,res=response)=>{
-    res.json({
-        ok:true
-    })
-}
-const postTramiteExterno =async(req=request,res=response)=>{
-    const body = req.body;
-    res.json({
-        ok:true,
-        body
-    })
-}
-const putTramiteExterno =async(req=request,res=response)=>{
-    res.json({
-        ok:true
-    })
-}
-const deleteTramiteExterno =async(req=request,res=response)=>{
-    res.json({
-        ok:true
-    })
-}
+      ok: true,
+      data,
+      countExter,
+      tramiteexter
+    });
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      msg: `Error al crear el tramite externo, error: ${error}`,
+    });
+  }
+};
+const putTramiteExterno = async (req = request, res = response) => {
+  res.json({
+    ok: true,
+  });
+};
+const deleteTramiteExterno = async (req = request, res = response) => {
+  res.json({
+    ok: true,
+  });
+};
 
 module.exports = {
-    getTramiteExternos,
-    getTramiteExterno,
-    postTramiteExterno,
-    putTramiteExterno,
-    deleteTramiteExterno
-}
+  getTramiteExternos,
+  getTramiteExterno,
+  postTramiteExterno,
+  putTramiteExterno,
+  deleteTramiteExterno,
+};
